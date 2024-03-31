41fbd31788a8772bee3840e5b45c2b44f6a3385b
David Julius <163961539+davidjs98@users.noreply.github.com>
2024-03-31 20:23:24 +0700

    Update tugas2.php

--- Tugas 1/tugas2.php
+++ Tugas 1/tugas2.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="mycss.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>forum input</title>
 </head>
 <body>
-    <form action="tugas1.php" method="post">
-        <label for="kode">kode:</label><br>
-        <input type="text" id="kode" name="kode"><br>
-        <label for="nama">nama:</label><br>
-        <input type="text" id="nama" name="nama"><br>
-        <label for="satuan">satuan:</label><br>
-        <input type="text" id="satuan" name="satuan"><br>
-        <label for="beli">beli:</label><br>
-        <input type="number" id="beli" name="beli"><br>
-        <label for="jual">jual:</label><br>
-        <input type="number" id="jual" name="jual"><br><br>
-        <input type="submit" value="Save" name="save">
-        <input type="submit" value="Cancel" name="cancel">
-    </form>
+    <div class="Rounded">
+        <form action="tugas1.php" method="post">
+            <label for="kode" class="lbl">kode:</label><br>
+            <input type="text" class="ipt" id="kode" name="kode"><br>
+            <label for="nama" class="lbl">nama:</label><br>
+            <input type="text" class="ipt" id="nama" name="nama"><br>
+            <label for="satuan" class="lbl">satuan:</label><br>
+            <input type="text" class="ipt" id="satuan" name="satuan"><br>
+            <label for="beli" class="lbl">beli:</label><br>
+            <input type="number" class="ipt" id="beli" name="beli"><br>
+            <label for="jual" class="lbl">jual:</label><br>
+            <input type="number" class="ipt" id="jual" name="jual"><br><br>
+            <button class="btn"><i class="fa fa-save"></i> save</button>
+            <button class="btn"><i class="fa fa-close"></i> cancel</button>
+        </form>
+    </div>  
 </body>
 </html>
